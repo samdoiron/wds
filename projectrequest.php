36c1510b272bb8f5bfa264da56755945a803e6f9
Gilberto De Melo <gilberto@innetwork.net>
2014-10-11 00:02:18 -0300

    added project request page

---
+++ projectrequest.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+    <html lang="en">
+        <?php include('head.php'); ?>
+        <body>
+            <?php $projectrequest = true ?>
+            <?php include('navbar.php') ?>
+            <!-- begin page content -->
+
+            <div class="container">
+                <div class="page-header">
+                    <h1>Project Request</h1>
+                    <p class="lead">Any member of the Dalhousie community is able to submit a request for a website, web application, or mobile application. Please read below for details.</p>
+                </div>
+
+                <h3>Websites</h3>
+                <p>In order to have a website, it is necessary to apply for a society username at http://www.dsu.ca/forms. Meanwhile,
+                    you can contact us with the specifications of your website and we will start working at it. Please be away that you are
+                    responsible for providing all the content of the website: descriptions, pictures, etc.
+                </p>
+                <p>If your society is not ratified with the DSU, we can still help you. Please contact us for details.</p>
+
+                <h3>Web / Mobile Applications</h3>
+                <p>All websites created by the WDS are responsive and can be viewed on computers, tablets, and mobile phones. Furthermore, we are able
+                    to create interactive mobile applications using the same technologies we use to create websites. <strong>However, at this moment we are not able to create iOS/Android mobile applications.</strong>
+                </p>
+
+                <br/>
+                <h4>Technologies we currently use:</h4>
+                <ul>
+                    <li>HTML5</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>Twitter Bootstrap</li>
+                    <li>PHP</li>
+                    <li>Laravel (PHP Framework)</li>
+                    <li>MySQL</li>
+                </ul>
+
+            </div>
+        </body>
+    </html>
